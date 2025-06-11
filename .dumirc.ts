cc596bfd8aad0288a061274ad313086189b2fea0
lby <lby@123>
2025-06-11 19:30:42 +0800

    build: base

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+  base: '/react-form',				 
+  publicPath: '/react-form/', 
   themeConfig: {
     name: 'react-form',
     logo: false,
